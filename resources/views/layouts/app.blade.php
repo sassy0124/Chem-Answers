e50921ba2a4fd2993d275943eb38933d20169905
sassy0124 <satoshi01060373@gmail.com>
2022-10-03 12:21:19 +0900

    cssとviewの編集

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- <title>{{ config('app.name', 'Laravel') }}</title> -->
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -18,6 +18,9 @@
     
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    
+    <!-- Design -->
+        <link rel="stylesheet" href="{{ asset('css/template.css') }}">
 
 </head>
 <body>
