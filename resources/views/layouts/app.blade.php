7c2b728cc4e21415352bcb1b94a96d90376b02d4
sassy0124 <satoshi01060373@gmail.com>
2022-10-14 19:58:04 +0900

    マイページに遷移するためのaタグの修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,7 +49,7 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="user">
+                                    <a class="dropdown-item" href="/user">
                                         マイページ
                                     </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
